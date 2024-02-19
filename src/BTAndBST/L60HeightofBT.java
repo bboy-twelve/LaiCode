4098851da3b9f2f1ea55bd1f48ce858de7420eda
jia sun <985060683@qq.com>
2024-02-19 15:03:46 -0500

    Update README.md

--- src/BTAndBST/L60HeightofBT.java
+++ src/BTAndBST/L60HeightofBT.java
@@ -1,4 +1,15 @@
 package BTAndBST;
 
 public class L60HeightofBT {
+    public int findHeight(TreeNode root) {
+        // Write your solution here
+        if(root == null){
+            return 0 ;
+        }
+        int leftOfHeight =  findHeight(root.left);
+        int rightOfHeight = findHeight(root.right);
+        return  Math.max(leftOfHeight,rightOfHeight) +1;
+    }
+
+
 }
